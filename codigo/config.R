0c8c0c343288eff1acdfc7396bc813234078a3d6
Leonardo Fujisima <leonardo@fujisimaanalytics.com>
2025-12-11 08:52:38 -0300

    Inclui renv

---
+++ codigo/config.R
@@ -0,0 +1,11 @@
+#=======================
+#= Importa bibliotecas =
+#=======================
+
+library(data.table)    # importa e trata arquivos .csv
+
+#==============================
+#= Importa dados do Saeb 2023 = 
+#==============================
+
+
