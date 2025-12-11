a07efb643960a50657b09661bcbc807388b02a97
Leonardo Fujisima <leonardo@fujisimaanalytics.com>
2025-12-11 08:58:50 -0300

    Importação de microdados do Saeb

--- codigo/config.R
+++ codigo/config.R
@@ -3,9 +3,15 @@
 #=======================
 
 library(data.table)    # importa e trata arquivos .csv
+library(here)          # facilita inclusão de caminho
 
 #==============================
 #= Importa dados do Saeb 2023 = 
 #==============================
+
+# Caminho para os microdados do 9º ano do Ensino Fundamental (2023)
+caminho_saeb_9_ef_23 <- here("dados", "TS_ALUNO_9EF.csv")
+# Importação dos dados utilizando data.table 
+saeb_9_ef_23 <- data.table::fread(caminho_saeb_9_ef_23, encoding="Latin-1")
 
 
