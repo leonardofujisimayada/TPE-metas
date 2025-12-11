fa0ac6492dc77232b4cc6cb8101e2de3c196d22a
Leonardo Fujisima <leonardo@fujisimaanalytics.com>
2025-12-11 15:09:18 -0300

    Inclui análise de 2019

--- codigo/config.R
+++ codigo/config.R
@@ -7,12 +7,21 @@ library(here)          # facilita inclusão de caminho
 library(dplyr)         # trata dataframes/datatables
 library(writexl)       # exporta arquivo para Excel
 
-#==============================
-#= Importa dados do Saeb 2023 = 
-#==============================
+#=========================
+#= Importa dados do Saeb = 
+#=========================
 
+# Caminho para os microdados do 9º ano do Ensino Fundamental (2019)
+caminho_saeb_9_ef_19 <- here("dados", "SAEB_2019", "TS_ALUNO_9EF.csv")
+# Caminho para os microdados do 9º ano do Ensino Fundamental (2021)
+caminho_saeb_9_ef_21 <- here("dados", "SAEB_2021", "TS_ALUNO_9EF.csv")
 # Caminho para os microdados do 9º ano do Ensino Fundamental (2023)
 caminho_saeb_9_ef_23 <- here("dados", "SAEB_2023", "TS_ALUNO_9EF.csv")
+
+# Importação dos dados utilizando data.table 
+saeb_9_ef_19 <- data.table::fread(caminho_saeb_9_ef_19, encoding="Latin-1")
+# Importação dos dados utilizando data.table 
+saeb_9_ef_21 <- data.table::fread(caminho_saeb_9_ef_21, encoding="Latin-1")
 # Importação dos dados utilizando data.table 
 saeb_9_ef_23 <- data.table::fread(caminho_saeb_9_ef_23, encoding="Latin-1")
 
---
+++ codigo/main-19.R
@@ -0,0 +1,90 @@
+#==================================================
+#= 1. Análise de proficiência por raça/cor - 2019 =
+#==================================================
+
+saeb_9_ef_19_racacor <- saeb_9_ef_19[
+  # Consistente entre os dados da aplicação do Saeb 2019 com o Censo da 
+  # Educação Básica 2019 finalizado
+  IN_SITUACAO_CENSO == 1 &
+    # Escola pública
+    IN_PUBLICA == 1,
+  .(
+    # Inclusão das opções de resposta para a pergunta: Qual é a sua cor ou 
+    # raça?
+    RACA_COR = case_when(
+      TX_RESP_Q002 == "A" ~ "Branca.",
+      TX_RESP_Q002 == "B" ~ "Preta.",
+      TX_RESP_Q002 == "C" ~ "Parda.",
+      TX_RESP_Q002 == "D" ~ "Amarela.",
+      TX_RESP_Q002 == "E" ~ "Indígena.",
+      TX_RESP_Q002 == "F" ~ "Não quero declarar.",
+      TX_RESP_Q002 == "*" ~ "Nulo",
+      TX_RESP_Q002 == "." ~ "Branco",
+      .default = NA
+    ),
+    # Média ponderada da proficiência em matemática
+    MEDIA_MT = sum(PROFICIENCIA_MT_SAEB, na.rm=T)/sum(PESO_ALUNO_MT, na.rm=T),
+    # Média ponderada da proficiência em língua portuguesa
+    MEDIA_LP = sum(PROFICIENCIA_LP_SAEB, na.rm=T)/sum(PESO_ALUNO_LP, na.rm=T)
+  ),
+  # Agrupamento por raça/cor
+  by = "TX_RESP_Q002"
+][
+  ,
+  .(
+    # Inclusão de variáveis de ano do Saeb e ano/série dos estudantes
+    ANO_SAEB = 2019, ANO_SERIE = 9, RACA_COR, MEDIA_MT, MEDIA_LP
+  )
+]
+
+#================================================================
+#= 1.1. Análise de proficiência por raça/cor e município - 2019 =
+#================================================================
+
+saeb_9_ef_19_racacor_municipio <- saeb_9_ef_19[
+  # Consistente entre os dados da aplicação do Saeb 2019 com o Censo da 
+  # Educação Básica 2019 finalizado
+  IN_SITUACAO_CENSO == 1 &
+    # Escola pública
+    IN_PUBLICA == 1,
+  .(
+    # Inclusão das opções de resposta para a pergunta: Qual é a sua cor ou 
+    # raça?
+    RACA_COR = case_when(
+      TX_RESP_Q002 == "A" ~ "Branca.",
+      TX_RESP_Q002 == "B" ~ "Preta.",
+      TX_RESP_Q002 == "C" ~ "Parda.",
+      TX_RESP_Q002 == "D" ~ "Amarela.",
+      TX_RESP_Q002 == "E" ~ "Indígena.",
+      TX_RESP_Q002 == "F" ~ "Não quero declarar.",
+      TX_RESP_Q002 == "*" ~ "Nulo",
+      TX_RESP_Q002 == "." ~ "Branco",
+      .default = NA
+    ),
+    # Média ponderada da proficiência em matemática
+    MEDIA_MT = sum(PROFICIENCIA_MT_SAEB, na.rm=T)/sum(PESO_ALUNO_MT, na.rm=T),
+    # Média ponderada da proficiência em língua portuguesa
+    MEDIA_LP = sum(PROFICIENCIA_LP_SAEB, na.rm=T)/sum(PESO_ALUNO_LP, na.rm=T)
+  ),
+  # Agrupamento por raça/cor
+  by = c("ID_MUNICIPIO", "TX_RESP_Q002")
+][
+  ,
+  .(
+    # Inclusão de variáveis de ano do Saeb e ano/série dos estudantes
+    ANO_SAEB = 2019, ANO_SERIE = 9, ID_MUNICIPIO, RACA_COR, MEDIA_MT, MEDIA_LP
+  )
+]
+
+#==========================
+#= 2. Exportação de dados =
+#==========================
+
+# Criação de lista com dataframes
+lista_dfs <- list(
+  TB_RACACOR_BRASIL    = saeb_9_ef_19_racacor,
+  TB_RACACOR_MUNICIPIO = saeb_9_ef_19_racacor_municipio
+)
+
+# Salva tudo em um único arquivo Excel
+write_xlsx(lista_dfs, path = here("dados", "TB_DESIGUALDADE_2019.xlsx"))
