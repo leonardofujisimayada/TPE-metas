c98e0d3f511598b476e391e1f96ac8ce26a0dad2
Leonardo Fujisima <leonardo@fujisimaanalytics.com>
2025-12-11 09:40:53 -0300

    Ajusta caminho dos arquivos

--- codigo/config.R
+++ codigo/config.R
@@ -11,7 +11,7 @@ library(dplyr)         # trata dataframes/datatables
 #==============================
 
 # Caminho para os microdados do 9º ano do Ensino Fundamental (2023)
-caminho_saeb_9_ef_23 <- here("dados", "TS_ALUNO_9EF.csv")
+caminho_saeb_9_ef_23 <- here("dados", "SAEB_2023", "TS_ALUNO_9EF.csv")
 # Importação dos dados utilizando data.table 
 saeb_9_ef_23 <- data.table::fread(caminho_saeb_9_ef_23, encoding="Latin-1")
 
