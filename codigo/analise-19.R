d447294b3a4aab0d3c7b9b43ff685d70b01e45e7
Leonardo Fujisima <leonardo@fujisimaanalytics.com>
2025-12-12 12:06:44 -0300

    Inclui microdados antigos de 2019

--- codigo/analise-19.R
+++ codigo/analise-19.R
@@ -88,3 +88,72 @@ lista_dfs <- list(
 
 # Salva tudo em um único arquivo Excel
 write_xlsx(lista_dfs, path = here("dados", "TB_DESIGUALDADE_2019.xlsx"))
+
+#=======================================================================
+#= 3. Análise de proficiência por raça/cor - 2019 (microdados antigos) =
+#=======================================================================
+
+saeb_9_ef_19_racacor_dta <- saeb_19_dta[
+  # Consistente entre os dados da aplicação do Saeb 2019 com o Censo da 
+  # Educação Básica 2019 finalizado
+  in_situacao_censo == 1 &
+    # Escola pública
+    in_publica == 1 &
+    # 9º ano do Ensino Fundamental
+    id_serie == 9,
+  .(
+    # Média ponderada da proficiência em matemática
+    MEDIA_MT = sum(proficiencia_mt_saeb, na.rm=T)/sum(peso_aluno_mt, na.rm=T),
+    # Média ponderada da proficiência em língua portuguesa
+    MEDIA_LP = sum(proficiencia_lp_saeb, na.rm=T)/sum(peso_aluno_lp, na.rm=T)
+  ),
+  # Agrupamento por raça/cor
+  by = "cor_raca"
+][
+  ,
+  .(
+    # Inclusão de variáveis de ano do Saeb e ano/série dos estudantes
+    ANO_SAEB = 2019, ANO_SERIE = 9, cor_raca, MEDIA_MT, MEDIA_LP
+  )
+]
+
+#====================================================================================
+#= 3.1 Análise de proficiência por raça/cor e município - 2019 (microdados antigos) =
+#====================================================================================
+
+saeb_9_ef_19_racacor_municipio_dta <- saeb_19_dta[
+  # Consistente entre os dados da aplicação do Saeb 2019 com o Censo da 
+  # Educação Básica 2019 finalizado
+  in_situacao_censo == 1 &
+    # Escola pública
+    in_publica == 1 &
+    # 9º ano do Ensino Fundamental
+    id_serie == 9,
+  .(
+    # Média ponderada da proficiência em matemática
+    MEDIA_MT = sum(proficiencia_mt_saeb, na.rm=T)/sum(peso_aluno_mt, na.rm=T),
+    # Média ponderada da proficiência em língua portuguesa
+    MEDIA_LP = sum(proficiencia_lp_saeb, na.rm=T)/sum(peso_aluno_lp, na.rm=T)
+  ),
+  # Agrupamento por raça/cor
+  by = c("id_municipio", "cor_raca")
+][
+  ,
+  .(
+    # Inclusão de variáveis de ano do Saeb e ano/série dos estudantes
+    ANO_SAEB = 2019, ANO_SERIE = 9, cor_raca, MEDIA_MT, MEDIA_LP
+  )
+]
+
+#==========================
+#= 4. Exportação de dados =
+#==========================
+
+# Criação de lista com dataframes
+lista_dfs <- list(
+  TB_RACACOR_BRASIL    = saeb_9_ef_19_racacor_dta,
+  TB_RACACOR_MUNICIPIO = saeb_9_ef_19_racacor_municipio_dta
+)
+
+# Salva tudo em um único arquivo Excel
+write_xlsx(lista_dfs, path = here("dados", "TB_DESIGUALDADE_2019_COM_MUNICIPIO.xlsx"))
--- codigo/config.R
+++ codigo/config.R
@@ -6,10 +6,14 @@ library(data.table)    # importa e trata arquivos .csv
 library(here)          # facilita inclusão de caminho
 library(dplyr)         # trata dataframes/datatables
 library(writexl)       # exporta arquivo para Excel
+library(haven)         # importa dados .dta
 
 #=========================
 #= Importa dados do Saeb = 
 #=========================
+
+# Caminho para os microdados ANTIGOS do 9º ano do Ensino Fundamental (2019)
+caminho_saeb_19_dta <- here("dados", "saeb_2019.dta")
 
 # Caminho para os microdados do 9º ano do Ensino Fundamental (2019)
 caminho_saeb_9_ef_19 <- here("dados", "SAEB_2019", "TS_ALUNO_9EF.csv")
@@ -17,6 +21,10 @@ caminho_saeb_9_ef_19 <- here("dados", "SAEB_2019", "TS_ALUNO_9EF.csv")
 caminho_saeb_9_ef_21 <- here("dados", "SAEB_2021", "TS_ALUNO_9EF.csv")
 # Caminho para os microdados do 9º ano do Ensino Fundamental (2023)
 caminho_saeb_9_ef_23 <- here("dados", "SAEB_2023", "TS_ALUNO_9EF.csv")
+
+# Importação dos microdados ANTIGOS do 9º ano do Ensino Fundamental (2019)
+saeb_19_dta <- haven::read_dta(caminho_saeb_19_dta)
+saeb_19_dta <- setDT(saeb_19_dta)
 
 # Importação dos dados utilizando data.table 
 saeb_9_ef_19 <- data.table::fread(caminho_saeb_9_ef_19, encoding="Latin-1")
